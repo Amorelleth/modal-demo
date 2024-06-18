171f5cd74c6a883dbad275c62012c272c0cacc24
Anna Kozlovskaia <amorelleth@gmail.com>
2024-06-18 17:05:44 +0200

    feat: describe Button unit tests

---
+++ src/ui/Button/Button.test.tsx
@@ -0,0 +1,14 @@
+import { describe, it } from "vitest";
+
+describe("App", () => {
+  it("has correct role", () => {});
+  it("renders text", () => {});
+  it("renders icon", () => {});
+  it("defaults to type 'button'", () => {});
+  it("applies disabled prop", () => {});
+  it("is not clickable if disabled", () => {});
+  it("passes correct aria-disabled", () => {});
+  it("renders text with icon", () => {});
+  it("passes aria-label", () => {});
+  it("calls onClick when clicked", () => {});
+});
