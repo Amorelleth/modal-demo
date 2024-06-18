d3440364b529d93f74e136c2f3fcfcba4086b28e
Anna Kozlovskaia <amorelleth@gmail.com>
2024-06-19 00:37:47 +0200

    feat: add Modal tests

--- src/ui/Modal/Modal.test.tsx
+++ src/ui/Modal/Modal.test.tsx
@@ -1,63 +1,320 @@
-import { describe, it } from "vitest";
+import type { RefObject } from "react";
 
-describe("ModalHeader", () => {
-  it("renders title", () => {});
-  it("passes labelId", () => {});
-  it("passes custom className", () => {});
-  it("renders close button", () => {});
-  it("calls onClose when close button clicked", () => {});
-});
+import { render } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
 
-describe("ModalFooter", () => {
-  it("renders action button if passed", () => {});
-  it("renders dismiss button if passed", () => {});
-  it("passes custom className", () => {});
-  it("calls action.onClick when action button clicked", () => {});
-  it("calls onClose after action.onClick when action button clicked", () => {});
-  it("calls dismiss.onClick when dismiss button clicked", () => {});
-  it("calls onClose after dismiss.onClick when dismiss button clicked", () => {});
-});
+import { expect, describe, it, vi } from "vitest";
 
-describe("ModalProvider", () => {
-  it("renders children", () => {});
-  it("passes value to context", () => {});
-  it("context.container defaults to document.body", () => {});
-});
+import { Modal } from "./Modal";
+import { ModalProvider } from "./Provider";
 
-describe("useFocusTrap", () => {
-  it("returns first focusable child", () => {});
-  it("returns handleTab", () => {});
-  it("returns handleTab working as expected", () => {});
-});
+import * as modalFooterModule from "./Footer";
+import * as modalHeaderModule from "./Header";
 
 describe("Modal", () => {
-  it("renders null when isOpen is false", () => {});
-  it("renders when isOpen is true", () => {});
-  it("renders children", () => {});
-  it("renders to the container from context", () => {});
+  it("renders null when isOpen is false", () => {
+    const container = document.createElement("div");
+    document.body.append(container);
 
-  // a11y
-  it("has correct role", () => {});
-  it("passes aria-modal true", () => {});
-  it("passes aria-describedby equal to children container id if children is string", () => {});
-  it("passes aria-labelledby equal to labelId passed to ModalHeader");
-  it("focuses first focusable element after opening", () => {});
-  it("calls onClose on outside click", () => {});
-  // keyboard
-  it("traps focus inside the modal on tab press", () => {});
-  it("traps focus inside the modal on shift tab press", () => {});
-  it("returns focus to triggerRef after closing", () => {});
-  it("calls onClose on pressing escape", () => {});
+    render(
+      <>
+        <ModalProvider container={container}>
+          <Modal isOpen={false} title="Title" onClose={vi.fn()}>
+            children
+          </Modal>
+        </ModalProvider>
+      </>
+    );
+    expect(container).toBeEmptyDOMElement();
+  });
 
-  // classNames
-  it("passes contentClassName to content wrapper", () => {});
-  it("passes footerClassName to ModalFooter", () => {});
-  it("passes headerClassName to ModalHeader", () => {});
+  it("renders when isOpen is true", () => {
+    const container = document.createElement("div");
+    document.body.append(container);
 
-  // passing props to child components
-  it("passes title to ModalHeader", () => {});
-  it("passes onClose to ModalHeader", () => {});
-  it("passes onClose to ModalFooter", () => {});
-  it("renders ModalFooter if not empty footer prop passed", () => {});
-  it("passes footer prop to ModalFooter", () => {});
+    render(
+      <>
+        <ModalProvider container={container}>
+          <Modal isOpen title="Title" onClose={vi.fn()}>
+            children
+          </Modal>
+        </ModalProvider>
+      </>
+    );
+    expect(container).not.toBeEmptyDOMElement();
+  });
+
+  it("renders children when open", () => {
+    const { getByText } = render(
+      <Modal isOpen title="Title" onClose={vi.fn()}>
+        children
+      </Modal>
+    );
+    getByText("children");
+  });
+
+  /**
+   * Checking a11y tests
+   */
+  it("has correct role", () => {
+    const { getByRole } = render(
+      <Modal isOpen title="Title" onClose={vi.fn()}>
+        children
+      </Modal>
+    );
+    getByRole("dialog");
+  });
+
+  it("passes aria-modal true", () => {
+    const { getByRole } = render(
+      <Modal isOpen title="Title" onClose={vi.fn()}>
+        children
+      </Modal>
+    );
+    expect(getByRole("dialog")).toHaveAttribute("aria-modal", "true");
+  });
+
+  it("focuses first focusable element after opening", () => {
+    const { getByLabelText } = render(
+      <Modal
+        isOpen
+        title="Title"
+        onClose={vi.fn()}
+        ariaProps={{ closeButton: { label: "close" } }}
+      >
+        children
+      </Modal>
+    );
+    expect(getByLabelText("close")).toHaveFocus();
+  });
+
+  it("calls onClose on pressing escape", async () => {
+    const onClose = vi.fn();
+    render(
+      <Modal
+        isOpen
+        title="Title"
+        onClose={onClose}
+        ariaProps={{ closeButton: { label: "close" } }}
+      >
+        children
+      </Modal>
+    );
+
+    await userEvent.keyboard("{Escape}");
+    await vi.waitFor(() => expect(onClose).toBeCalled(), { timeout: 600 });
+  });
+
+  it("returns focus to triggerRef after closing", async () => {
+    const focus = vi.fn();
+
+    render(
+      <Modal
+        isOpen
+        title="Title"
+        triggerRef={{ current: { focus } } as unknown as RefObject<HTMLElement>}
+        onClose={() => {}}
+        ariaProps={{ closeButton: { label: "close" } }}
+      >
+        children
+      </Modal>
+    );
+
+    await userEvent.keyboard("{Escape}");
+    await vi.waitFor(() => expect(focus).toBeCalled(), { timeout: 600 });
+  });
+
+  it("calls onClose on outside click", async () => {
+    const onClose = vi.fn();
+
+    const { getByRole } = render(
+      <>
+        <div>outside element</div>
+        <Modal
+          isOpen
+          title="Title"
+          onClose={onClose}
+          ariaProps={{ closeButton: { label: "close" } }}
+        >
+          children
+        </Modal>
+      </>
+    );
+
+    getByRole("dialog").parentElement?.click();
+    await vi.waitFor(() => expect(onClose).toBeCalled(), { timeout: 600 });
+  });
+
+  it("passes aria-describedby", () => {
+    const { getByRole } = render(
+      <Modal
+        isOpen
+        title="Title"
+        ariaProps={{ modal: { describedby: "description" } }}
+        onClose={vi.fn()}
+      >
+        <div id="description">description</div>
+      </Modal>
+    );
+    expect(getByRole("dialog")).toHaveAccessibleDescription("description");
+  });
+
+  it("passes aria-labelledby", () => {
+    const { getByRole } = render(
+      <Modal
+        isOpen
+        title="Title"
+        ariaProps={{ modal: { labelledby: "label" } }}
+        onClose={vi.fn()}
+      >
+        <div id="label">label</div>
+      </Modal>
+    );
+    expect(getByRole("dialog")).toHaveAccessibleName("label");
+  });
+
+  it("defaults aria-describedby equal to children container id if children is string", () => {
+    const { getByRole } = render(
+      <Modal isOpen title="Title" onClose={vi.fn()}>
+        children
+      </Modal>
+    );
+    expect(getByRole("dialog")).toHaveAccessibleDescription("children");
+  });
+
+  it("defaults aria-labelledby equal to labelId passed to ModalHeader", () => {
+    const { getByRole } = render(
+      <Modal isOpen title="Title" onClose={vi.fn()}>
+        children
+      </Modal>
+    );
+    expect(getByRole("dialog")).toHaveAccessibleName("Title");
+  });
+
+  /**
+   * Child components tests
+   */
+  it("renders ModalHeader, passing all the props", () => {
+    const ModalHeaderSpy = vi
+      .spyOn(modalHeaderModule, "ModalHeader")
+      .mockReturnValue(<></>);
+
+    render(
+      <Modal
+        isOpen
+        title="Title"
+        onClose={vi.fn()}
+        headerClassName="customClassName"
+        ariaProps={{ closeButton: { label: "Close" } }}
+      >
+        children
+      </Modal>
+    );
+
+    expect(ModalHeaderSpy).toHaveBeenCalledWith(
+      expect.objectContaining({
+        title: "Title",
+        className: "customClassName",
+        buttonAriaLabel: "Close",
+      }),
+      expect.anything()
+    );
+
+    ModalHeaderSpy.mockRestore();
+  });
+
+  it("renders ModalFooter, passing all the props", () => {
+    const ModalFooterSpy = vi
+      .spyOn(modalFooterModule, "ModalFooter")
+      .mockReturnValue(<></>);
+
+    render(
+      <Modal
+        isOpen
+        title="Title"
+        onClose={vi.fn()}
+        footer={{
+          action: { text: "Action" },
+          dismiss: { text: "Close" },
+        }}
+        footerClassName="customClassName"
+      >
+        children
+      </Modal>
+    );
+
+    expect(ModalFooterSpy).toHaveBeenCalledWith(
+      expect.objectContaining({
+        action: { text: "Action" },
+        dismiss: { text: "Close" },
+        className: "customClassName",
+      }),
+      expect.anything()
+    );
+
+    ModalFooterSpy.mockRestore();
+  });
+
+  it("does not render ModalFooter if footer.action and footer.dismiss are not passed", () => {
+    const ModalFooterSpy = vi
+      .spyOn(modalFooterModule, "ModalFooter")
+      .mockReturnValue(<></>);
+
+    render(
+      <Modal isOpen title="Title" onClose={vi.fn()} footer={{}}>
+        children
+      </Modal>
+    );
+    expect(ModalFooterSpy).not.toHaveBeenCalled();
+
+    ModalFooterSpy.mockRestore();
+  });
+
+  /**
+   * Passing classNames tests
+   */
+  it("passes contentClassName", () => {
+    render(
+      <Modal
+        isOpen
+        title="Title"
+        onClose={vi.fn()}
+        contentClassName="contentClassName"
+      >
+        children
+      </Modal>
+    );
+    expect(
+      document.body.querySelector(".contentClassName")
+    ).toBeInTheDocument();
+  });
+
+  it("passes footerClassName", () => {
+    render(
+      <Modal
+        isOpen
+        title="Title"
+        onClose={vi.fn()}
+        footerClassName="footerClassName"
+        footer={{ action: { text: "text" } }}
+      >
+        children
+      </Modal>
+    );
+    expect(document.body.querySelector(".footerClassName")).toBeInTheDocument();
+  });
+
+  it("passes headerClassName", () => {
+    render(
+      <Modal
+        isOpen
+        title="Title"
+        onClose={vi.fn()}
+        headerClassName="headerClassName"
+      >
+        children
+      </Modal>
+    );
+    expect(document.body.querySelector(".headerClassName")).toBeInTheDocument();
+  });
 });
