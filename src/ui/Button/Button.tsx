379c40e0009424fac7e11921d2608bac26266cf8
Anna Kozlovskaia <amorelleth@gmail.com>
2024-06-18 12:42:39 +0200

    feat: add disabled button styles, ref forwarding and ariaLabel

--- src/ui/Button/Button.tsx
+++ src/ui/Button/Button.tsx
@@ -1,5 +1,5 @@
 import { clsx } from "clsx";
-import type { SVGProps } from "react";
+import { forwardRef, type ForwardedRef, type SVGProps } from "react";
 
 import styles from "./Button.module.css";
 
@@ -8,33 +8,44 @@ export type ButtonProps = {
   shape?: "pill" | "rounded";
   text?: string;
   type?: "submit" | "reset" | "button";
+  disabled?: boolean;
+  ariaLabel?: string;
   onClick?: () => void;
   icon?: (props: SVGProps<SVGSVGElement>) => JSX.Element;
 };
 
-export const Button = ({
-  variant = "default",
-  shape = "rounded",
-  text,
-  type,
-  onClick,
-  icon: Icon,
-  ...props
-}: ButtonProps) => {
-  return (
-    <button
-      {...props}
-      type={type}
-      onClick={onClick}
-      className={clsx(styles.button, {
-        [styles.action]: variant === "action",
-        [styles.default]: variant === "default",
-        [styles.pill]: shape === "pill",
-        [styles.rounded]: shape === "rounded",
-      })}
-    >
-      {Icon && <Icon />}
-      {text}
-    </button>
-  );
-};
+export const Button = forwardRef(
+  (
+    {
+      variant = "default",
+      shape = "rounded",
+      text,
+      type,
+      disabled = false,
+      onClick,
+      icon: Icon,
+      ariaLabel,
+    }: ButtonProps,
+    ref?: ForwardedRef<HTMLButtonElement>
+  ) => {
+    return (
+      <button
+        ref={ref}
+        type={type}
+        aria-label={ariaLabel}
+        aria-disabled={disabled}
+        disabled={disabled}
+        onClick={onClick}
+        className={clsx(styles.button, {
+          [styles.action]: variant === "action",
+          [styles.default]: variant === "default",
+          [styles.pill]: shape === "pill",
+          [styles.rounded]: shape === "rounded",
+        })}
+      >
+        {Icon && <Icon />}
+        {text}
+      </button>
+    );
+  }
+);
