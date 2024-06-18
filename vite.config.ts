fea45407049306c935e4203b4a3d34e8d081611d
Anna Kozlovskaia <amorelleth@gmail.com>
2024-06-19 00:39:19 +0200

    feat: setup test coverage

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,26 @@ export default defineConfig({
   test: {
     environment: "jsdom",
     setupFiles: "./src/tests/setup.ts",
-    include: ["./src/**/*.test.tsx?(x)"],
+    include: ["./src/**/*.test.ts?(x)"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+      reportsDirectory: "./.coverage",
+      exclude: [
+        "node_modules/",
+
+        // tests
+        "src/tests/setup.ts",
+        "src/**/*.test.ts?(x)",
+
+        // configs
+        ".eslintrc.cjs",
+        "postcss.config.mjs",
+
+        // app root
+        "src/main.tsx",
+        "src/components/App/index.ts",
+      ],
+    },
   },
 });
